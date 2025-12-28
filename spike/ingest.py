5c985114b7bfcc36b2d11e6c1d9d620a14e58394
Chien-Yu Lin <cy20lin@gmail.com>
2025-12-28 23:42:04 +0800

    chore(spike): Ingest with globbber, indexer and upserter

--- spike/ingest.py
+++ spike/ingest.py
@@ -1,25 +1,60 @@
 from pkms.globber import PathspecGlobber
 from pkms.indexer import HtmlIndexer
+from pkms.upserter import Sqlite3Upserter
 import json
 
 
-globber_config = PathspecGlobber.Config(patterns=["**/*.html"], negate=False)
+globber_config = PathspecGlobber.Config(
+    patterns=["**/*.html"], 
+    negate=False
+)
 globber = PathspecGlobber(config=globber_config)
 
 html_indexer_config = HtmlIndexer.Config()
 html_indexer = HtmlIndexer(config=html_indexer_config)
 
-file_locations = globber.glob(".")
+upserter_config = Sqlite3Upserter.Config(
+    db_path='spike-pkms.db'
+)
+upserter = Sqlite3Upserter(config=upserter_config)
+
+collection_path = '.'
+file_locations = globber.glob(collection_path)
+
+# print('')    
+# print("=== BEGIN index ===")
+# print('')    
+
+# indexed_document = None
+# documents = []
+# for file_location in file_locations:
+#     try:
+#         indexed_document = html_indexer.index(file_location)
+#         print(f'success index: {repr(file_location.path)}, id: {indexed_document.file_id}')
+#         documents.append(indexed_document)
+#     except Exception as e:
+#         print(f'skipped index: {repr(file_location.path)}, reason: {e}')
+#         # print(e.with_traceback())
+# print('')    
+# print("=== BEGIN upsert ===")
+# print('')    
+
+# for indexed_document in documents:
+#     try:
+#         upserter.upsert(indexed_document)
+#         print(f'success upsert: {indexed_document.file_id}')
+#     except Exception as e:
+#         print(f'skipped upsert: {indexed_document.file_id}, reason: {e}')
 
 
 indexed_document = None
+documents = []
 for file_location in file_locations:
     try:
         indexed_document = html_indexer.index(file_location)
-        print(f'indexed {repr(file_location.path)}, id: {indexed_document.file_id}')
+        upserter.upsert(indexed_document)
+        print(f'success index: {repr(file_location.path)}, id: {indexed_document.file_id}')
+        documents.append(indexed_document)
     except Exception as e:
-        print(f'skipped {repr(file_location.path)}, reason: {e}')
-        print(e.with_traceback())
-
-
-# print(indexed_document.model_dump_json())
+        print(f'skipped index: {repr(file_location.path)}, reason: {e}')
+        # print(e.with_traceback())
