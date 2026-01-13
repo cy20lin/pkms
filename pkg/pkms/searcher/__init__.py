03a09246326271e570bce3c2debd63103cbba5f6
Chien-Yu Lin <cy20lin@gmail.com>
2026-01-13 23:19:18 +0800

    fix(searcher): Add Sqlite3Searcher classes to the searcher namespace

--- pkg/pkms/searcher/__init__.py
+++ pkg/pkms/searcher/__init__.py
@@ -1,1 +1,10 @@
-from ._Sqlite3Searcher import Sqlite3Searcher
+from ._Sqlite3Searcher import (
+    Sqlite3Searcher,
+    Sqlite3SearcherConfig,
+    Sqlite3SearcherRuntime
+)
+__all__ = [
+    Sqlite3Searcher,
+    Sqlite3SearcherConfig,
+    Sqlite3SearcherRuntime
+]
