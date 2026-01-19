35bace32d6e7cbb080f4b55a625416b620bc8827
Chien-Yu Lin <cy20lin@gmail.com>
2026-01-19 14:51:10 +0800

    feat: Add MarkdownIndexer and OdtIndexer classes

---
+++ pkg/pkms/component/indexer/_OdtIndexer.py
@@ -0,0 +1,73 @@
+from typing import Literal,Optional
+from pkms.core.model import FileLocation
+from pkms.core.model import FileStamp
+from pkms.core.model import IndexedDocument
+from pkms.core.component.indexer import (
+    Indexer,
+    IndexerConfig,
+    IndexerRuntime
+)
+from pkms.core.utility import *
+
+from urllib.parse import urljoin, urlparse, parse_qsl, urlunparse, urlencode
+import inscriptis
+import datetime
+import logging
+import os
+
+from pkms.lib.odt_to_html import (
+    OdtToHtmlConverter,
+    OdtToHtmlConverterConfig,
+    OdtToHtmlConverterRuntime,
+)
+from bs4 import BeautifulSoup
+
+class OdtIndexerConfig(IndexerConfig):
+    type: Literal['OdtIndexerConfig'] = 'OdtIndexerConfig'
+    converter: OdtToHtmlConverterConfig
+
+class OdtIndexerRuntime(IndexerRuntime):
+    def __init__(self, *args, config: OdtIndexerConfig, **kwargs):
+        self.converter = OdtToHtmlConverterRuntime(config=config.converter)
+
+class OdtIndexer(Indexer):
+    Config = OdtIndexerConfig
+    Runtime = OdtIndexerRuntime
+
+    def __init__(self, config: OdtIndexerConfig, runtime: Optional[OdtIndexerRuntime] = None):
+        if runtime is None:
+            runtime = OdtIndexerRuntime(config=config)
+        super().__init__(config=config, runtime=runtime)
+        self.converter = OdtToHtmlConverter(config=config.converter, runtime=runtime.converter)
+
+    def index(self, file_location: FileLocation, file_stamp: FileStamp) -> IndexedDocument:
+        assert file_location.scheme == 'file'
+        assert file_location.authority == ''
+        assert os.path.isabs(file_location.path)
+        # NOTE: MAYBE support relative path in future, now enforce absoulte path
+        # TODO: expose title in the converter
+        content = self.converter.convert(file_location.path, title=None)
+        soup = BeautifulSoup(content, "lxml")
+        title = soup.title.text
+        text = inscriptis.get_text(content)
+        now = datetime.datetime.now()
+        index = IndexedDocument(**{
+            "index_created_datetime": now.astimezone().isoformat(),
+            "index_updated_datetime": now.astimezone().isoformat(),
+            "file_created_datetime": file_stamp.created_datetime,
+            "file_modified_datetime": file_stamp.modified_datetime,
+            "file_id": file_stamp.id,
+            "file_uid": file_stamp.uid,
+            "file_uri": file_location.uri,
+            "file_hash_sha256": file_stamp.hash_sha256,
+            "file_size": file_stamp.size,
+            "file_extension": file_stamp.extension,
+            "file_kind": file_stamp.kind,
+            # Select a good title, fallback if needed
+            "title": title if title else file_stamp.title,
+            "importance": file_stamp.importance,
+            "origin_uri": None,
+            "text": text,
+            "extra": {},
+        })
+        return index
