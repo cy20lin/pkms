251e30301c90f9636650384c691932b9b72cde12
Chien-Yu Lin <cy20lin@gmail.com>
2026-01-19 16:09:12 +0800

    feat(web): Enable html and markdown view via API, add middleware for no-referrer

--- pkg/pkms/web/__init__.py
+++ pkg/pkms/web/__init__.py
@@ -5,9 +5,10 @@ import os
 import urllib
 import argparse
 import sqlite3
+import pathlib
 
 from fastapi import FastAPI, Depends, HTTPException, Query
-from fastapi.responses import FileResponse, JSONResponse, RedirectResponse
+from fastapi.responses import FileResponse, JSONResponse, RedirectResponse, HTMLResponse
 import uvicorn
 
 from pkms.core.component.searcher import (
@@ -67,10 +68,59 @@ def file_uri_to_path(uri: str) -> str:
 # print(file_uri_to_path(posix_uri))   # /home/user/documents/test file.txt
 # print(file_uri_to_path(windows_uri)) # C:\Users\User\Documents\test file.txt
 
+from pkms.lib.markdown_to_html import MarkdownToHtmlConverter
+from pkms.lib.odt_to_html import OdtToHtmlConverter
+
+class HtmlRepresenter():
+
+    def __init__(self):
+        md_config = MarkdownToHtmlConverter.Config(
+            title=None,
+            title_from_metadata=True,
+            title_from_h1=True,
+            title_fallback=None,
+            title_from_filename=True,
+            redirect_base="/redirect?target="
+        )
+        self.md_converter = MarkdownToHtmlConverter(config=md_config)
+        odt_config = OdtToHtmlConverter.Config(
+            title=None,
+            title_from_metadata=True,
+            title_from_h1=True,
+            title_fallback=None,
+            title_from_styled_title=True,
+            title_from_filename=True,
+            show_page_breaks=False,
+        )
+        self.odt_converter = OdtToHtmlConverter(config=odt_config)
+
+    def represent(self, file_path):
+        path = pathlib.Path(file_path)
+        if path.suffix == '.odt':
+            result = self.odt_converter.convert(file_path,title=None)
+        elif path.suffix == '.md':
+            result = self.md_converter.convert(file_path,title=None)
+        return result
+
+# --------- Middleware ------------
+
+from fastapi import FastAPI, Request
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import Response
+
+class HeaderMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        response = await call_next(request)
+        response.headers["Referrer-Policy"] = "no-referrer"
+        # response.headers["Content-Security-Policy"] = "default-src 'self'"
+        return response
+
+
 # ---------- App Factory ----------
 
-def create_app(searcher: "Searcher", resolver: "UriResolver") -> FastAPI:
+def create_app(searcher: "Searcher", resolver: "UriResolver", representer: HtmlRepresenter) -> FastAPI:
     app = FastAPI(title="PKMS Search WebApp")
+    app.add_middleware(HeaderMiddleware)
 
     @app.get("/")
     def index():
@@ -101,8 +151,12 @@ def create_app(searcher: "Searcher", resolver: "UriResolver") -> FastAPI:
 
             if not os.path.exists(file_path):
                 raise FileNotFoundError(file_path)
-
-            return FileResponse(file_path)
+            
+            if file_id.endswith('.html'):
+                return FileResponse(file_path)
+            else:
+                representation: str = representer.represent(file_path)
+                return HTMLResponse(representation)
 
         except Exception:
             raise HTTPException(
@@ -120,17 +174,6 @@ def create_app(searcher: "Searcher", resolver: "UriResolver") -> FastAPI:
         return RedirectResponse(url=target, status_code=302)
 
     return app
-
-
-# ---------- Composition Root ----------
-
-def build_searcher(db_path: str) -> "Searcher":
-    config = Sqlite3Searcher.Config(
-        db_path=db_path,
-        max_limit=100,
-    )
-    return Sqlite3Searcher(config=config)
-
 
 # ---------- CLI / Entrypoint ----------
 
@@ -170,9 +213,15 @@ def parse_args(argv: Optional[list[str]] = None) -> argparse.Namespace:
 def main(argv: Optional[list[str]] = None) -> None:
     args = parse_args(argv)
 
-    searcher = build_searcher(db_path=args.db_path)
-    resolver = UriResolver(args.db_path)
-    app = create_app(searcher, resolver)
+    searcher_config = Sqlite3Searcher.Config(
+        db_path=args.db_path,
+        max_limit=100,
+    )
+    searcher = Sqlite3Searcher(config=searcher_config)
+    resolver_config = UriResolver.Config(db_path = args.db_path)
+    resolver = UriResolver(config=resolver_config)
+    representer = HtmlRepresenter()
+    app = create_app(searcher, resolver, representer)
 
     uvicorn.run(
         app,
