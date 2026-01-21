13ddd9920a4457a0aab5cb69c2165fad8cf45692
Chien-Yu Lin <cy20lin@gmail.com>
2026-01-21 13:33:11 +0800

    feat: Make pkms executable and add routing to cli and web modules

--- pkg/pkms/__init__.py
+++ pkg/pkms/__init__.py
@@ -1,1 +1,23 @@
 __version__=''
+from . import cli
+from . import web
+
+def main(argv: list[str] | None = None) -> int:
+    from pkms.core.utility import CommandParser
+    args = argv[1:] if argv is not None else []
+    parser = CommandParser(
+        name="pkms",
+        description="Personal Knowledge Management System"
+    )
+    parser.add_command("cli", cli.main, "Command Line Interface")
+    parser.add_command("web", web.main, "Web User Interface")
+    parsed_args = parser.parse(args)
+    command = parser.get_command(parsed_args.command)
+    code = command(args)
+    return code
+
+if __name__ == '__main__':
+    import sys
+    argv = sys.argv
+    code = main(argv)
+    sys.exit(code)
