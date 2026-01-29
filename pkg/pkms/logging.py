c83d53d840541d8601a245b9a274cc322b546f86
Chien-Yu Lin <cy20lin@gmail.com>
2026-01-29 16:47:54 +0800

    refactor(logging): Add NullLogger for dummy logging

--- pkg/pkms/logging.py
+++ pkg/pkms/logging.py
@@ -18,3 +18,13 @@ class InterceptHandler(logging.Handler):
             depth=depth,
             exception=record.exc_info,
         ).log(level, record.getMessage())
+    
+class NullLogger:
+    def __init__(self,/, *args, **kwargs): pass
+    def debug(self, /, *a, **kw): pass
+    def info(self, /, *a, **kw): pass
+    def warning(self, /, *a, **kw): pass
+    def error(self, /, *a, **kw): pass
+    def critical(self, /, *a, **kw): pass
+
+default_null_logger = NullLogger()
